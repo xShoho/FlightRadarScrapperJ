399b52903b8fe3828965a2f3735b84d5a6b20660
Kamil Starosta <ks@g3w.eu>
2025-05-17 18:30:49 +0200

    Added URL Scrapper to Scrapper.java

--- src/main/java/com/flightradarscrapper/service/Scrapper.java
+++ src/main/java/com/flightradarscrapper/service/Scrapper.java
@@ -1,26 +1,58 @@
 package com.flightradarscrapper.service;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Scrapper {
     private final List<FlightData> flights = new ArrayList<>();
-    private List<String> urls = new ArrayList<>();
+    private List<String> arrival_urls = new ArrayList<>();
+    private List<String> departure_urls = new ArrayList<>();
     private final WebDriver driver = new FirefoxDriver();
 
-    public Scrapper(List<String> urls) {
-        this.urls = urls;
+    public List<FlightData> scrapper(boolean is_arrival) {
+        if(is_arrival) {
+            for(String url: arrival_urls) {
+                this.scrape(url);
+            }
+        } else {
+            for(String url: departure_urls) {
+                this.scrape(url);
+            }
+        }
+        return this.flights;
     }
 
-    public void scrape() {
-        for(String url : urls) {
-            // Establish Connection
-            driver.get(url);
+    private void scrape(String url) {
+        // Establish Connection
+        this.driver.get(url);
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
 
+
+
+    }
+
+    public void getUrls() {
+        // Establish Connection
+        this.driver.get("https://www.flightradar24.com/data/airports/poland");
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
+
+        List<WebElement> tbody = driver.findElements(By.name("tbody"));
+
+        if(!tbody.isEmpty()) {
+            List<WebElement> links = tbody.get(0).findElements(By.name("a"));
+
+            if(!links.isEmpty()) {
+                for(WebElement link: links) {
+                    this.arrival_urls.add(link.getAttribute("href") + "/arrivals");
+                    this.departure_urls.add(link.getAttribute("href") + "/departures");
+                }
+            }
         }
     }
 }
